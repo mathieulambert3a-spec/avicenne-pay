9a919dea57b3b3efe07ee24206cb22331df6b27a
mathieulambert3a-spec <mathieu.lambert.3a@gmail.com>
2026-03-01 10:20:38 +0000

    Suppression complète du contenu

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User, Role, Filiere, Annee, Site, Programme, MATIERES
-from app.models.mission import Mission
-from app.models.sub_mission import SousMission
-from app.models.declaration import Declaration, StatutDeclaration
-from app.models.ligne_declaration import LigneDeclaration
--- app/schemas/auth.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-
-class LoginForm(BaseModel):
-    email: str
-    password: str
